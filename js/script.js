4015772f0eb6fef575e10908e9aef5b8f1904ccf
Ethan Joseph <ethancredo.ascc@outlook.com>
2025-10-25 17:37:27 +0800

    add getHumanChoice function and print choice using switch case

--- js/script.js
+++ js/script.js
@@ -13,3 +13,24 @@ if (computerChoice === 0) {
 } else {
     console.log("Invalid computer choice.");
 }
+
+function getHumanChoice() {
+    return parseInt(prompt("Type 1 = Rock, 2 = Paper, 3 = Scissors"));
+}
+
+let humanChoice = getHumanChoice();
+
+switch (humanChoice) {
+    case 1:
+        console.log("Rock");
+        break;
+    case 2:
+        console.log("Paper");
+        break;
+    case 3:
+        console.log("Scissors");
+        break;
+    default:
+        console.log("Invalid choice");
+        break;
+}
