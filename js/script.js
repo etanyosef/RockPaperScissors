b335d2fc68b05e411cc5eb6d9111a51bdf74acef
Ethan Joseph <ethancredo.ascc@outlook.com>
2025-10-25 17:23:35 +0800

    add index and js file, create getComputerChoice function

---
+++ js/script.js
@@ -0,0 +1,15 @@
+function getComputerChoice() {
+    return Math.floor(Math.random() * 3);
+}
+
+let computerChoice = getComputerChoice();
+
+if (computerChoice === 0) {
+    console.log("Rock");
+} else if (computerChoice === 1) {
+    console.log("Paper");
+} else if (computerChoice === 2) {
+    console.log("Scissors");
+} else {
+    console.log("Invalid computer choice.");
+}
